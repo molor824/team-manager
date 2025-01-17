bdd4b7a72fa7a25bb66d40c1e8683804fc98434f
Molor Margad-Erdene <molormargaderdene@gmail.com>
2025-01-17 13:18:57 +0800

    feat: add user context and user drop menu

--- src/App.tsx
+++ src/App.tsx
@@ -7,71 +7,86 @@ import {
 import { Button, Layout, Menu } from "antd";
 import { Content, Header } from "antd/es/layout/layout";
 import Sider from "antd/es/layout/Sider";
-import { useState } from "react";
+import { createContext, useState } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import UserProfile from "./components/UserProfile";
 
 const ROUTES = [
   {
-    path: "/",
+    key: "/",
     icon: <HomeFilled />,
     label: "Home",
   },
   {
-    path: "/todo",
+    key: "/todo",
     icon: <ProfileFilled />,
     label: "Todo",
   },
   {
-    path: "/report",
+    key: "/report",
     icon: <SlidersFilled />,
     label: "Reports",
   },
 ];
 
+export type User = {
+  username: string;
+  profileUrl?: string;
+  email: string;
+  phoneNumber?: string;
+};
+export const UserContext = createContext<
+  [User | null, (user: User | null) => void]
+>(null as any);
+
 export default function App() {
   const [collapsed, setCollapsed] = useState(false);
   const { pathname } = useLocation();
   const navigate = useNavigate();
+  const [user, setUser] = useState<User | null>({
+    username: "jone_the_crazy",
+    email: "jone_the_crazy@me.com",
+    phoneNumber: "+123456789",
+  });
 
   return (
-    <Layout className="w-screen min-h-screen">
-      <Header className="bg-white flex items-center justify-between gap-8">
-        <div className="flex items-center gap-8">
-          <Button
-            type="text"
-            size="large"
-            icon={<MenuOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-          />
-          <h1 className="text-3xl font-bold">Team Manager</h1>
-        </div>
-        <UserProfile />
-      </Header>
-      <Layout>
-        <Sider
-          trigger={null}
-          theme="light"
-          width={200}
-          className="overflow-hidden px-4"
-          collapsed={collapsed}
-          collapsible
-        >
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={[pathname]}
-            items={ROUTES.map(({ path, icon, label }) => ({
-              key: path,
-              icon,
-              label,
-              onClick: () => navigate(path),
-            }))}
-          />
-        </Sider>
-        <Content className="p-8">
-          <Outlet />
-        </Content>
+    <UserContext.Provider value={[user, setUser]}>
+      <Layout className="w-screen min-h-screen">
+        <Header className="bg-white flex items-center justify-between gap-8">
+          <div className="flex items-center gap-8">
+            <Button
+              type="text"
+              size="large"
+              icon={<MenuOutlined />}
+              onClick={() => setCollapsed(!collapsed)}
+            />
+            <h1 className="text-3xl font-bold">Team Manager</h1>
+          </div>
+          <UserProfile />
+        </Header>
+        <Layout>
+          <Sider
+            trigger={null}
+            theme="light"
+            width={200}
+            className="overflow-hidden px-4"
+            collapsed={collapsed}
+            collapsible
+          >
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={[pathname]}
+              items={ROUTES.map((route) => ({
+                ...route,
+                onClick: () => navigate(route.key),
+              }))}
+            />
+          </Sider>
+          <Content className="p-8">
+            <Outlet />
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </UserContext.Provider>
   );
 }
--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,33 +1,58 @@
-import { UserOutlined } from "@ant-design/icons";
-import { Button } from "antd";
+import {
+  EditFilled,
+  UserDeleteOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Button, Dropdown, Space } from "antd";
+import { useNavigate } from "react-router-dom";
+import { User, UserContext } from "../App";
+import { useContext } from "react";
 
-export type User = {
-  username: string;
-  profileUrl?: string;
-};
-export type Props = {
-  user?: User;
-};
-export default function UserProfile({ user }: Props) {
+export default function UserProfile() {
+  const [user, setUser] = useContext(UserContext);
+  const getProfile = (user: User) =>
+    user.profileUrl ? (
+      <img
+        src={user.profileUrl}
+        alt={user.username}
+        className="object-cover max-w-[30px]"
+      />
+    ) : (
+      <UserOutlined alt={user.username} />
+    );
+  const navigate = useNavigate();
+  const menuItems = [
+    {
+      key: "edit",
+      icon: <EditFilled />,
+      label: "Edit profile",
+      onClick: () => navigate("/edit"),
+    },
+    {
+      key: "signout",
+      icon: <UserDeleteOutlined />,
+      label: "Sign out",
+      danger: true,
+      onClick: () => {
+        setUser(null);
+        navigate("/signup");
+      },
+    },
+  ];
+
   return user ? (
-    <Button
-      type="text"
-      size="large"
-      className="overflow-hidden flex items-center gap-4"
-    >
-      <p className="font-bold">{user.username}</p>
-      {user.profileUrl ? (
-        <img
-          src={user.profileUrl}
-          alt={user.username}
-          className="object-cover max-w-[30px]"
-        />
-      ) : (
-        <UserOutlined alt={user.username} />
-      )}
-    </Button>
+    <>
+      <Dropdown menu={{ items: menuItems }} trigger={["click"]}>
+        <Button type="text" size="large">
+          <Space>
+            <p className="font-bold">{user.username}</p>
+            {getProfile(user)}
+          </Space>
+        </Button>
+      </Dropdown>
+    </>
   ) : (
-    <Button type="primary" size="large">
+    <Button type="primary" size="large" onClick={() => navigate("/signup")}>
       Sign Up
     </Button>
   );
