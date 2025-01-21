79fe814a8a34fb65b409d3bfef475647508ec5a0
Molor Margad-Erdene <molormargaderdene@gmail.com>
2025-01-21 14:20:40 +0800

    fix: finish login page

--- src/page/LogInPage.tsx
+++ src/page/LogInPage.tsx
@@ -1,18 +1,47 @@
 import { Button, Card, Form, Input } from "antd";
-import { getApi } from "../tools/fetchApi";
+import { useState } from "react";
+import { postApi } from "../tools/fetchApi";
+import { useUser } from "../components/UserProvider";
+import { useNavigate } from "react-router-dom";
 
 export default function LogInPage() {
+  const [loading, setLoading] = useState(false);
+  const [invalidAccount, setInvalidAccount] = useState(false);
+  const { setToken } = useUser();
+  const navigate = useNavigate();
   const handleOnFinish = (value: any) => {
+    setLoading(true);
+    setInvalidAccount(false);
     console.log(value);
+    postApi("/auth/login", value)
+      .then((res) => {
+        console.log(res);
+        setToken(res.token);
+        navigate("/");
+      })
+      .catch((_) => {
+        setLoading(false);
+        setInvalidAccount(true);
+      });
   };
 
   return (
     <Card>
-      <Form autoComplete="off" layout="vertical" onFinish={handleOnFinish}>
+      <Form
+        autoComplete="off"
+        layout="vertical"
+        onFinish={handleOnFinish}
+        disabled={loading}
+      >
+        <p
+          className="text-red-500"
+          style={{ display: invalidAccount ? "block" : "none" }}
+        >
+          Invalid account or password
+        </p>
         <Form.Item
           label="E-Mail"
           name="email"
-          validateFirst
           rules={[
             {
               type: "email",
@@ -21,13 +50,6 @@ export default function LogInPage() {
             {
               required: true,
               message: "The input is required!",
-            },
-            {
-              validator: (_, value) =>
-                getApi(`/auth/exists?user=${value}`).then((res) =>
-                  res === true ? Promise.resolve() : Promise.reject()
-                ),
-              message: "This E-Mail does not have an account!",
             },
           ]}
         >
@@ -47,7 +69,7 @@ export default function LogInPage() {
         </Form.Item>
 
         <Form.Item>
-          <Button type="primary" htmlType="submit">
+          <Button type="primary" htmlType="submit" loading={loading}>
             Log In
           </Button>
         </Form.Item>
