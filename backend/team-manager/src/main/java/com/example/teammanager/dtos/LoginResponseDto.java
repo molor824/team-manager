2b67f02d7a8a1c6a151d7ccfb0c6a1e5ba9c3344
FuduDufu <ggdulguun3@gmail.com>
2025-01-24 11:51:26 +0800

    fix

--- backend/team-manager/src/main/java/com/example/teammanager/dtos/LoginResponseDto.java
+++ backend/team-manager/src/main/java/com/example/teammanager/dtos/LoginResponseDto.java
@@ -3,4 +3,7 @@ package com.example.teammanager.dtos;
 import org.springframework.lang.NonNull;
 
 public record LoginResponseDto(@NonNull String token) {
+    public Object getBody() {
+        return null;
+    }
 }
