4e4a03eddb9be5ca3822d352c886e7dbab04fcda
FuduDufu <ggdulguun3@gmail.com>
2025-01-27 14:31:29 +0800

    fix: data type

--- backend/team-manager/src/main/java/com/example/teammanager/dtos/LoginResponseDto.java
+++ backend/team-manager/src/main/java/com/example/teammanager/dtos/LoginResponseDto.java
@@ -2,7 +2,7 @@ package com.example.teammanager.dtos;
 
 import org.springframework.lang.NonNull;
 
-public record LoginResponseDto(@NonNull String token) {
+public record LoginResponseDto(Long id, @NonNull String token) {
     public Object getBody() {
         return null;
     }
